7e1915f4880e7c743565c599e70dd7c9935ef061
Sebastian Valerio <jsvale.guerrero@gmail.com>
2019-09-16 13:14:52 -0700

    HTML, CSS & Little JS

---
+++ Code/app.js
@@ -0,0 +1,13 @@
+﻿var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var port = process.env.PORT || 1337;
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+http.listen(port, function () {
+    console.log('listening on *:' + port);
+});
